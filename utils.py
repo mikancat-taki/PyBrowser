ff54a93c2fb4e0d235347e598ef9a4991b8f0a03
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-31 23:01:22 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,27 @@
-# 将来的な履歴管理やお気に入り機能用
+# 履歴管理とお気に入り機能
+
 class HistoryManager:
     def __init__(self):
         self.history = []
 
     def add(self, url):
-        self.history.append(url)
+        if url not in self.history:
+            self.history.append(url)
 
     def get_history(self):
         return self.history
+
+class FavoritesManager:
+    def __init__(self):
+        self.favorites = []
+
+    def add(self, url):
+        if url not in self.favorites:
+            self.favorites.append(url)
+
+    def remove(self, url):
+        if url in self.favorites:
+            self.favorites.remove(url)
+
+    def get_all(self):
+        return self.favorites
